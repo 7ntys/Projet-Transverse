87ecea23d507b9e931b3c1883a42034dff2486ff
Datharr <61124632+Datharr@users.noreply.github.com>
2022-05-26 20:50:51 +0200

    Cleaning Code part 2

--- Terrain (16 x 16).tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="Terrain (16 x 16)" tilewidth="16" tileheight="16" tilecount="170" columns="17">
- <image source="tileset/Seasonal Tilesets/1 - Grassland/Terrain (16 x 16).png" width="272" height="160"/>
-</tileset>
